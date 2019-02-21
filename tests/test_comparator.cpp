b0f81f0910f8985add0ecd224747eb630dcdf918
Kondo Takeo <takeoverjp@gmail.com>
2019-02-21 12:56:10 +0900

    Add test for comparator.

---
+++ tests/test_comparator.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <stdio.h>
+#include <assert.h>
+
+#include <dtl/dtl.hpp>
+
+#include "comparator.h"
+
+static void
+test (const std::string& lhs, const std::string& rhs, const bool exp)
+{
+  DecNumberInsensitive c;
+  assert (exp == c.impl(lhs, rhs));
+}
+
+int
+main (void)
+{
+  test ("123: abc", "456: abc", true);
+  test ("123: abc", "456: abc", false);
+  return 0;
+}
